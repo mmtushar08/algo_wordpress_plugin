f243c1b17eda5407fec96f2f1bf7aaf5c1c6442b
Tushar Makwana <5483982+mmtushar08@users.noreply.github.com>
2025-09-24 14:58:51 +0530

    Create rsi.py File

---
+++ indicators/rsi.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def calculate_rsi(data, window=14):
+    delta = data.diff()
+    gain = (delta.where(delta > 0, 0)).fillna(0)
+    loss = (-delta.where(delta < 0, 0)).fillna(0)
+    
+    avg_gain = gain.rolling(window=window).mean()
+    avg_loss = loss.rolling(window=window).mean()
+    
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    
+    return rsi
